46b11dd707d26da4fbdfcc11372638b635340b2a
SofiaFredriksson <sofiapkf@gmail.com>
2019-01-21 16:45:00 +0000

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  if array.length == 1
+array.join()
+elsif array.length == 2
+  array.join(" and ")
+else
+last_element = array.pop()
+last_element.insert(0, ", and ")
 
+  new_arr = array.join(", ")
+  new_arr.concat(last_element)
+
+end
 end
